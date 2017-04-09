0121c64ba4b1b0358e068ca905db79f914de00d7
Francesco Gazzetta <francygazz@gmail.com>
2017-04-09 18:10:10 +0200

    added polyvariadic application

--- Data/Function/Polyvariadic.hs
+++ Data/Function/Polyvariadic.hs
@@ -2,8 +2,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Data.Function.Polyvariadic where
 
+import Data.Foldable
 import Data.Accumulator
 --import Data.Collections
+
+
+---- creation
 
 applyToAccumulatedArgs :: (Varargs a b x, Accumulator a i) => (a -> b) -> x
 applyToAccumulatedArgs = varargs empty
@@ -19,4 +23,21 @@ instance (Accumulator c i, Varargs c b x) => Varargs c b (i -> x) where
 
 instance Varargs accumulator result result where
   varargs a f = f a
+
+
+---- application
+
+class Apply a b x where
+  apply' :: x -> [a] -> b
+
+instance Apply a b b where
+  apply' f _ = f
+
+instance (Apply a b x) => Apply a b (a -> x) where
+  apply' f (x:xs) = apply (f x) xs
+  apply' _ _ = error "Not enough arguments in polyvariadic application"
+
+
+apply :: (Apply a b x, Foldable t) => x -> t a -> b
+apply f t = apply f $ toList t
 
