02a7cafd80d2d71ea185ee788a16a4d8afe9cf29
Francesco Gazzetta <francygazz@gmail.com>
2017-04-15 16:56:08 +0200

    some docs

--- Data/Accumulator.hs
+++ Data/Accumulator.hs
@@ -5,18 +5,26 @@ module Data.Accumulator where -- MAYBE rename to Accumulate
 
 import qualified Data.Set as Set
 
+-- | Lift a monoid into an Accumulator. This is a newtype because the mappend isn't always the ideal way of accumulating
 newtype AccumulatorMonoid a = AccumulatorMonoid {getAccumulatorMonoid :: a}
 
 instance Monoid m => Monoid (AccumulatorMonoid m) where
   mempty = AccumulatorMonoid mempty
   mappend (AccumulatorMonoid a) (AccumulatorMonoid b) = AccumulatorMonoid $ mappend a b
 
-
+-- | An 'Accumulator c i' supports creation of an empty accumulator of type c
+-- and accumulation of elements of type i in it.
+-- This is different from 'Monoid', where '<>' acts between two values with the same type.
 class Accumulator c i | c -> i where
-  insert :: i -> c -> c
-  empty :: c
-  singleton :: i -> c
-  singleton = flip insert empty
+  insert :: i -> c -> c -- ^ Accumulate a value
+  --MAYBE empty is not necessaryly empty. Rename to new or something?
+  --MAYBE remove entirely? Empty is not necessary anywhere in polyvariadic
+  --and introduces a functional dependency. which is bad.
+  empty :: c -- ^ Empty accumulator
+
+-- | Accumulate a single value
+singleton :: Accumulator c i => i -> c
+singleton = flip insert empty
 
 instance Accumulator [a] a where
   insert = (:)
--- Data/Function/Polyvariadic.hs
+++ Data/Function/Polyvariadic.hs
@@ -1,42 +1,80 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
-module Data.Function.Polyvariadic where
+module Data.Function.Polyvariadic
+  (
+    -- * Creation
+    Polyvariadic (polyvariadic)
+  , applyToAccumulatedArgs
+    -- * Application
+  , Apply (apply')
+  , apply
+  ) where
 
 import Data.Foldable
 import Data.Accumulator
---import Data.Collections
 
 
 ---- creation
+
+-- | Creation of functions with an arbitrary number of arguments.
+--
+-- The arguments will be accumulated in the given 'Accumulator',
+-- which will then be passed as an argument to the function.
+--
+-- ==== __Examples__
+-- TODO
+-- The classic @printf@ function, which takes an arbitrary amount of arguments
+-- and inserts them in a string.
+--
+-- @
+-- magicChar = \'%\'
+-- data PrintfAccum = PrintfAccum { done :: String, todo :: String }
+-- instance (Show x) => Accumulator PrintfAccum x where
+--   empty = ("","")
+--   insert x (PrintfAccum done (_:todo)) = PrintfAccum (done ++ show x ++ (takeWhile (\/= magicChar) todo)) (dropWhile (\/= magicChar) todo)
+--   insert x acc = acc
+--
+-- printf' str = polyvariadic (PrintfAccum (takeWhile (\/= magicChar) str) (dropWhile (\/= magicChar) str)) id
+-- @
+--
+-- >>> printf' "test%asd%12%3" 'A' 666 True
+class Polyvariadic accumulator result x where
+  polyvariadic :: accumulator -> (accumulator -> result) -> x
+
+-- | Accumulates the next argument
+instance (Accumulator c i, Polyvariadic c b x) => Polyvariadic c b (i -> x) where
+  polyvariadic a f x = polyvariadic (insert x a) f
+
+-- | There are no more arguments to accumulate so the function is applied
+-- to the 'Accumulator'
+instance Polyvariadic accumulator result result where
+  polyvariadic a f = f a
 
 applyToAccumulatedArgs :: (Polyvariadic a b x, Accumulator a i) => (a -> b) -> x
 applyToAccumulatedArgs = polyvariadic empty
 --argsToAccumulator :: Polyvariadic a a x => x
 --argsToAccumulator = applyToAccumulatedArgs id
 
-class Polyvariadic accumulator result x where
-  polyvariadic :: accumulator -> (accumulator -> result) -> x
-
-instance (Accumulator c i, Polyvariadic c b x) => Polyvariadic c b (i -> x) where
-  polyvariadic a f x = polyvariadic (insert x a) f
-
-instance Polyvariadic accumulator result result where
-  polyvariadic a f = f a
-
 
 ---- application
 
+-- | Application of function with an arbitrary number of arguments to the
+-- elements of a list.
+--
+-- __Will raise an error if the list doesn't have enough elements__
 class Apply a b x where
   apply' :: x -> [a] -> b
 
-instance Apply a b b where
-  apply' f _ = f
-
+-- | The final type is not reached yet and the application continues
 instance (Apply a b x) => Apply a b (a -> x) where
   apply' f (x:xs) = apply (f x) xs
   apply' _ _ = error "Not enough arguments in polyvariadic application"
 
+-- | The final type is reached and the application terminates
+instance Apply a b b where
+  apply' f _ = f
 
+-- | Like 'apply'' but with an arbitrary 'Foldable' instead if a list
 apply :: (Apply a b x, Foldable t) => x -> t a -> b
 apply f t = apply f $ toList t
 
